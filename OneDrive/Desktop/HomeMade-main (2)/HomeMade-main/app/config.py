b083597595636a4171db3c5f36cc43ed3bcc0c94
Hussein380 <huznigarane@gmail.com>
2024-12-26 12:14:55 +0300

    removed unnecesary folders

--- OneDrive/Desktop/HomeMade-main (2)/HomeMade-main/app/config.py
+++
@@ -1,8 +0,0 @@
-# app/config.py
-
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://HomeMade:hzg@746789@localhost/HomeMade'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecretkey')  # Use environment variable or default
